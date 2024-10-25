16dc294f8452dc239a8b305a54d47a7c54c83ca1
Brambarov <ivan.brambarov@gmail.com>
2024-10-25 08:48:30 +0300

    Add Motorcycle model

---
+++ api/Models/Motorcycle.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models
+{
+    public class Motorcycle
+    {
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+    }
+}
