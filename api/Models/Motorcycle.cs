6c2962ae99e5c63fb896a983576bf0e8cb07144c
Brambarov <ivan.brambarov@gmail.com>
2024-10-25 08:52:51 +0300

    Refactor Motorcycle model

--- api/Models/Motorcycle.cs
+++ api/Models/Motorcycle.cs
@@ -2,8 +2,8 @@
 {
     public class Motorcycle
     {
+        public int Id { get; set; }
         public string Make { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
-        public int Year { get; set; }
     }
 }
