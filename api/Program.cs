77c6493da87fbc376b595c431f5a9fed46b7e705
Brambarov <ivan.brambarov@gmail.com>
2024-10-25 09:14:22 +0300

    Add default connection string

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,7 @@
 
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace api
 {
     public class Program
@@ -13,6 +16,11 @@ namespace api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
