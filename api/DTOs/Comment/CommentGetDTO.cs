ef7aa61e20f007646cf178f73f8098f8547245f6
Brambarov <ivan.brambarov@gmail.com>
2024-10-27 19:27:06 +0200

    Add MotorcycleId & CreatedBy properties to CommentGetDTO

--- api/DTOs/Comment/CommentGetDTO.cs
+++ api/DTOs/Comment/CommentGetDTO.cs
@@ -4,6 +4,8 @@
     {
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+        public int? MotorcycleId { get; set; }
         public DateTime CreatedOn { get; set; }
+        public string? CreatedBy { get; set; } = string.Empty;
     }
 }
