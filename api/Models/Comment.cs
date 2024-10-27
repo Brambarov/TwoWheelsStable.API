f02ce2798c3889e63122c7256ef830ef2478d3e6
Brambarov <ivan.brambarov@gmail.com>
2024-10-27 19:30:18 +0200

    Add UserId & User properties to Comment model

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,11 +1,16 @@
-﻿namespace api.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
 {
+    [Table("Comments")]
     public class Comment
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string UserId { get; set; } = string.Empty;
+        public User? User { get; set; }
         public int? MotorcycleId { get; set; }
         public Motorcycle? Motorcycle { get; set; }
     }
