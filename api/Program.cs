f0c3a7a3f794a66919bb1b1b5f477efd79e076fc
Brambarov <ivan.brambarov@gmail.com>
2024-10-27 19:36:39 +0200

    Code cleanup

--- api/Program.cs
+++ api/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Security.Claims;
 
 namespace api
 {
