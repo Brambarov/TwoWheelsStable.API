933d1e5626f07cc511f083bf4dbf911b1b7c99ed
Brambarov <ivan.brambarov@gmail.com>
2024-10-29 22:52:27 +0200

    Register SpecsService into Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -109,9 +109,11 @@ namespace api
 
             builder.Services.AddScoped<IMotorcyclesRepository, MotorcyclesRepository>();
             builder.Services.AddScoped<ICommentsRepository, CommentsRepository>();
+            builder.Services.AddScoped<ISpecsRepository, SpecsRepository>();
             builder.Services.AddScoped<IMotorcyclesService, MotorcyclesService>();
             builder.Services.AddScoped<ICommentsService, CommentsService>();
             builder.Services.AddScoped<IUsersService, UsersService>();
+            builder.Services.AddScoped<ISpecsService, SpecsService>();
             builder.Services.AddScoped<IAPINinjasService, APINinjasService>();
 
             var app = builder.Build();
