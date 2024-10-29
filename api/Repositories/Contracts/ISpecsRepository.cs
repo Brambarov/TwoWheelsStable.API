f8c128792cd69dae7407831d77e74cc2d165564a
Brambarov <ivan.brambarov@gmail.com>
2024-10-29 22:52:56 +0200

    Create ISpecsRepository & SpecsRepository

---
+++ api/Repositories/Contracts/ISpecsRepository.cs
@@ -0,0 +1,9 @@
+﻿using api.Models;
+
+namespace api.Repositories.Contracts
+{
+    public interface ISpecsRepository
+    {
+        Task<Specs> GetAsync(string make, string model, int year);
+    }
+}
---
+++ api/Repositories/SpecsRepository.cs
@@ -0,0 +1,30 @@
+﻿using api.Data;
+using api.Models;
+using api.Repositories.Contracts;
+using api.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositories
+{
+    public class SpecsRepository : ISpecsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IAPINinjasService _aPINinjasService;
+
+        public SpecsRepository(ApplicationDbContext context,
+                               IAPINinjasService aPINinjasService)
+        {
+            _context = context;
+            _aPINinjasService = aPINinjasService;
+        }
+
+        public async Task<Specs> GetAsync(string make, string model, int year)
+        {
+            var specs = await _context.Specs.FirstOrDefaultAsync(s => s.make == make && s.model == model && s.year == year);
+
+            if (specs != null) return specs;
+
+            throw new ApplicationException("Specs not found!");
+        }
+    }
+}
