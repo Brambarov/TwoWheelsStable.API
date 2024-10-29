ed448562d285e1d28df54a21d97f76d705e01309
Brambarov <ivan.brambarov@gmail.com>
2024-10-29 22:52:14 +0200

    Create ISpecsService & SpecsService

---
+++ api/Services/Contracts/ISpecsService.cs
@@ -0,0 +1,9 @@
+﻿using api.Models;
+
+namespace api.Services.Contracts
+{
+    public interface ISpecsService
+    {
+        Task<Specs?> GetOrCreateAsync(string make, string model, int year);
+    }
+}
---
+++ api/Services/SpecsService.cs
@@ -0,0 +1,30 @@
+﻿using api.Models;
+using api.Repositories.Contracts;
+using api.Services.Contracts;
+
+namespace api.Services
+{
+    public class SpecsService : ISpecsService
+    {
+        private readonly ISpecsRepository _specsRepository;
+        private readonly IAPINinjasService _aPINinjasService;
+
+        public SpecsService(ISpecsRepository specsRepository,
+                            IAPINinjasService aPINinjasService)
+        {
+            _specsRepository = specsRepository;
+            _aPINinjasService = aPINinjasService;
+        }
+
+        public async Task<Specs?> GetOrCreateAsync(string make, string model, int year)
+        {
+            var specs = await _specsRepository.GetAsync(make, model, year);
+
+            if (specs != null) return specs;
+
+            specs = await _aPINinjasService.GetAsync(make, model, year);
+
+            throw new NotImplementedException();
+        }
+    }
+}
