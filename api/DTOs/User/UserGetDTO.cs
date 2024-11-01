693602ae47d1fa3e822cfb4ec660df044e3ede6e
Brambarov <ivan.brambarov@gmail.com>
2024-11-01 14:51:09 +0200

    Change properties to non-nullable strings

--- api/DTOs/User/UserGetDTO.cs
+++ api/DTOs/User/UserGetDTO.cs
@@ -2,8 +2,8 @@
 {
     public class UserGetDTO
     {
-        public string? UserName { get; set; } = string.Empty;
-        public string? Email { get; set; } = string.Empty;
-        public string? Token { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
     }
 }
