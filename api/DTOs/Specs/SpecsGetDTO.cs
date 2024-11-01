950c8e313c8b4bf28322c19ed4cf210003b736fb
Brambarov <ivan.brambarov@gmail.com>
2024-11-01 16:24:17 +0200

    Create SpecsGetDTO

---
+++ api/DTOs/Specs/SpecsGetDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace api.DTOs.Specs
+{
+    public class SpecsGetDTO
+    {
+        public string Type { get; set; } = string.Empty;
+        public string Displacement { get; set; } = string.Empty;
+        public string Engine { get; set; } = string.Empty;
+        public string Power { get; set; } = string.Empty;
+        public string Torque { get; set; } = string.Empty;
+        public string Compression { get; set; } = string.Empty;
+        public string BoreStroke { get; set; } = string.Empty;
+        public string ValvesPerCylinder { get; set; } = string.Empty;
+        public string FuelSystem { get; set; } = string.Empty;
+        public string FuelControl { get; set; } = string.Empty;
+        public string Ignition { get; set; } = string.Empty;
+        public string Lubrication { get; set; } = string.Empty;
+        public string Cooling { get; set; } = string.Empty;
+        public string Gearbox { get; set; } = string.Empty;
+        public string Transmission { get; set; } = string.Empty;
+        public string Clutch { get; set; } = string.Empty;
+        public string Frame { get; set; } = string.Empty;
+        public string FrontSuspension { get; set; } = string.Empty;
+        public string FrontWheelTravel { get; set; } = string.Empty;
+        public string RearSuspension { get; set; } = string.Empty;
+        public string RearWheelTravel { get; set; } = string.Empty;
+        public string FrontTire { get; set; } = string.Empty;
+        public string RearTire { get; set; } = string.Empty;
+        public string FrontBrakes { get; set; } = string.Empty;
+        public string RearBrakes { get; set; } = string.Empty;
+        public string TotalWeight { get; set; } = string.Empty;
+        public string SeatHeight { get; set; } = string.Empty;
+        public string TotalHeight { get; set; } = string.Empty;
+        public string TotalLength { get; set; } = string.Empty;
+        public string TotalWidth { get; set; } = string.Empty;
+        public string GroundClearance { get; set; } = string.Empty;
+        public string Wheelbase { get; set; } = string.Empty;
+        public string FuelCapacity { get; set; } = string.Empty;
+        public string Starter { get; set; } = string.Empty;
+    }
+}
