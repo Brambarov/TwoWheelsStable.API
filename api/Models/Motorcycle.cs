14aad815e6d1d9f47e48a6080c3f5131d4fe5775
Brambarov <ivan.brambarov@gmail.com>
2024-11-01 16:08:07 +0200

    Make SpecsId property nullable

--- api/Models/Motorcycle.cs
+++ api/Models/Motorcycle.cs
@@ -10,7 +10,7 @@ namespace api.Models
         public string Make { get; set; } = string.Empty;
         public string Model { get; set; } = string.Empty;
         public int Year { get; set; }
-        public int SpecsId { get; set; }
+        public int? SpecsId { get; set; }
         public Specs? Specs { get; set; }
         //public int? UserId { get; set; }
         //public User? User { get; set; }
