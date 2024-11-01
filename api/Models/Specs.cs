88c8a2c4258597f7c4c00996a4bd87b22189a4b5
Brambarov <ivan.brambarov@gmail.com>
2024-11-01 11:02:16 +0200

    Change Specs properties to start with capital letter

--- api/Models/Specs.cs
+++ api/Models/Specs.cs
@@ -6,42 +6,42 @@ namespace api.Models
     public class Specs
     {
         public int Id { get; set; }
-        public string make { get; set; } = string.Empty;
-        public string model { get; set; } = string.Empty;
-        public int year { get; set; }
-        public string type { get; set; } = string.Empty;
-        public string displacement { get; set; } = string.Empty;
-        public string engine { get; set; } = string.Empty;
-        public string power { get; set; } = string.Empty;
-        public string torque { get; set; } = string.Empty;
-        public string compression { get; set; } = string.Empty;
-        public string bore_stroke { get; set; } = string.Empty;
-        public string valves_per_cylinder { get; set; } = string.Empty;
-        public string fuel_system { get; set; } = string.Empty;
-        public string fuel_control { get; set; } = string.Empty;
-        public string ignition { get; set; } = string.Empty;
-        public string lubrication { get; set; } = string.Empty;
-        public string cooling { get; set; } = string.Empty;
-        public string gearbox { get; set; } = string.Empty;
-        public string transmission { get; set; } = string.Empty;
-        public string clutch { get; set; } = string.Empty;
-        public string frame { get; set; } = string.Empty;
-        public string front_suspension { get; set; } = string.Empty;
-        public string front_wheel_travel { get; set; } = string.Empty;
-        public string rear_suspension { get; set; } = string.Empty;
-        public string rear_wheel_travel { get; set; } = string.Empty;
-        public string front_tire { get; set; } = string.Empty;
-        public string rear_tire { get; set; } = string.Empty;
-        public string front_brakes { get; set; } = string.Empty;
-        public string rear_brakes { get; set; } = string.Empty;
-        public string total_weight { get; set; } = string.Empty;
-        public string seat_height { get; set; } = string.Empty;
-        public string total_height { get; set; } = string.Empty;
-        public string total_length { get; set; } = string.Empty;
-        public string total_width { get; set; } = string.Empty;
-        public string ground_clearance { get; set; } = string.Empty;
-        public string wheelbase { get; set; } = string.Empty;
-        public string fuel_capacity { get; set; } = string.Empty;
-        public string starter { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Displacement { get; set; } = string.Empty;
+        public string Engine { get; set; } = string.Empty;
+        public string Power { get; set; } = string.Empty;
+        public string Torque { get; set; } = string.Empty;
+        public string Compression { get; set; } = string.Empty;
+        public string Bore_stroke { get; set; } = string.Empty;
+        public string Valves_per_cylinder { get; set; } = string.Empty;
+        public string Fuel_system { get; set; } = string.Empty;
+        public string Fuel_control { get; set; } = string.Empty;
+        public string Ignition { get; set; } = string.Empty;
+        public string Lubrication { get; set; } = string.Empty;
+        public string Cooling { get; set; } = string.Empty;
+        public string Gearbox { get; set; } = string.Empty;
+        public string Transmission { get; set; } = string.Empty;
+        public string Clutch { get; set; } = string.Empty;
+        public string Frame { get; set; } = string.Empty;
+        public string Front_suspension { get; set; } = string.Empty;
+        public string Front_wheel_travel { get; set; } = string.Empty;
+        public string Rear_suspension { get; set; } = string.Empty;
+        public string Rear_wheel_travel { get; set; } = string.Empty;
+        public string Front_tire { get; set; } = string.Empty;
+        public string Rear_tire { get; set; } = string.Empty;
+        public string Front_brakes { get; set; } = string.Empty;
+        public string Rear_brakes { get; set; } = string.Empty;
+        public string Total_weight { get; set; } = string.Empty;
+        public string Seat_height { get; set; } = string.Empty;
+        public string Total_height { get; set; } = string.Empty;
+        public string Total_length { get; set; } = string.Empty;
+        public string Total_width { get; set; } = string.Empty;
+        public string Ground_clearance { get; set; } = string.Empty;
+        public string Wheelbase { get; set; } = string.Empty;
+        public string Fuel_capacity { get; set; } = string.Empty;
+        public string Starter { get; set; } = string.Empty;
     }
 }
