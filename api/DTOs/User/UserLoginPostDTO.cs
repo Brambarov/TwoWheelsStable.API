348bd079e7663e1ed27de53ad646fb3d7c6426c7
Brambarov <ivan.brambarov@gmail.com>
2024-11-01 14:52:57 +0200

    Change properties to non-nullable string

--- api/DTOs/User/UserLoginPostDTO.cs
+++ api/DTOs/User/UserLoginPostDTO.cs
@@ -5,8 +5,8 @@ namespace api.DTOs.User
     public class UserLoginPostDTO
     {
         [Required]
-        public string? UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }
