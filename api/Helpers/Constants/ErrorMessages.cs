df93d36040369d128eb70910d441328fa67ac408
Brambarov <ivan.brambarov@gmail.com>
2024-11-17 18:48:46 +0200

    Move ErrorMessages

--- api/Helpers/Constants/ErrorMessages.cs
+++ api/Helpers/Constants/ErrorMessages.cs
@@ -1,14 +1,14 @@
-﻿namespace api.Helpers
+﻿namespace api.Helpers.Constants
 {
     public static class ErrorMessages
     {
+        public const string UnexpectedError = "Unexpected error occurred!";
         public const string RegistrationError = "Registration failed!";
         public const string LoginError = "Login failed!";
         public const string UserNameOrPasswordIncorrectError = "Username/password is incorrect!";
         public const string UnauthorizedError = "Unauthorized access!";
         public const string HttpConnectionError = "Http connection failed!";
         public const string ExternalApiError = "Error occured while calling an external API!";
-        public const string UnexpectedError = "Unexpected error occurred!";
         public const string JWTSigningKeyError = "JWT Signing key exception!";
         public const string SoftDeletionError = "{0} deletion failed!";
         public const string TokenCreationError = "{0} token creation failed!";
@@ -16,5 +16,6 @@
         public const string PropertyIsInvalidError = "{0} {1} is invalid!";
         public const string NotFoundError = "{0} not found!";
         public const string NotFoundOnError = "{0} not found on {1}!";
+        public const string UnhandledExceptionError = "Unhandled exception occurred!";
     }
 }
