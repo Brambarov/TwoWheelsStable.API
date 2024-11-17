39bb6efd2aa1184e54ee010414e4723b7173e711
Brambarov <ivan.brambarov@gmail.com>
2024-11-17 21:37:56 +0200

    Add GetAll, GetById, Update, & Delete endpoints to UsersController

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using api.DTOs.User;
+using api.Helpers.Queries;
 using api.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -9,6 +11,22 @@ namespace api.Controllers
     public class UsersController(IUsersService usersService) : ControllerBase
     {
         private readonly IUsersService _usersService = usersService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] UserQuery query)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(await _usersService.GetAllAsync(query));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(await _usersService.GetByIdAsync(id));
+        }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserRegisterPostDTO dto)
@@ -26,12 +44,23 @@ namespace api.Controllers
             return Ok(await _usersService.LoginAsync(dto));
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById([FromRoute] string id)
+        [Authorize]
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] string id,
+                                                [FromBody] UserPutDTO putDto)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            return Ok(await _usersService.GetByIdAsync(id));
+            return Ok(await _usersService.UpdateAsync(id, putDto));
+        }
+
+        [Authorize]
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(await _usersService.DeleteAsync(id));
         }
     }
 }
