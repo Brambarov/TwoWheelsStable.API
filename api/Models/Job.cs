bc036c6a7ed590d8a9d4deb23ccb58da83eef88c
Brambarov <ivan.brambarov@gmail.com>
2024-11-22 15:23:36 +0200

    Create Job model

---
+++ api/Models/Job.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    [Table("Jobs")]
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Cost { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Mileage { get; set; }
+        public int DueMileage { get; set; }
+        public int? MotorcycleId { get; set; }
+        public Motorcycle? Motorcycle { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
