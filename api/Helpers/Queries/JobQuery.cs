d67028ae6bf06b9db1cce95c9cd012fb1f1e7c75
Brambarov <ivan.brambarov@gmail.com>
2024-11-22 22:21:46 +0200

    Add properties to JobQuery

--- api/Helpers/Queries/JobQuery.cs
+++ api/Helpers/Queries/JobQuery.cs
@@ -2,6 +2,10 @@
 {
     public class JobQuery
     {
+        public string? Title { get; set; } = null;
+        public string? Description { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDescending { get; set; } = false;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
     }
