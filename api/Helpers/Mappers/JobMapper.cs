3a4212c9347f8268996532abe6a65b07b00006a1
Brambarov <ivan.brambarov@gmail.com>
2024-11-22 16:31:09 +0200

    Create JobMapper & JobQuery

---
+++ api/Helpers/Mappers/JobMapper.cs
@@ -0,0 +1,53 @@
+﻿using api.DTOs.Job;
+using api.Models;
+
+namespace api.Helpers.Mappers
+{
+    public static class JobMapper
+    {
+        public static JobGetDTO ToGetDTO(this Job model)
+        {
+            return new JobGetDTO
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Cost = model.Cost,
+                Date = model.Date,
+                DueDate = model.DueDate,
+                Mileage = model.Mileage,
+                DueMileage = model.DueMileage
+            };
+        }
+
+        public static Job FromPostDTO(this JobPostDTO dto, string userId, int? motorcycleId)
+        {
+            return new Job
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Cost = dto.Cost,
+                Date = dto.Date,
+                DueDate = dto.DueDate,
+                Mileage = dto.Mileage,
+                DueMileage = dto.DueMileage,
+                UserId = userId,
+                MotorcycleId = motorcycleId
+            };
+        }
+
+        public static Job FromPutDTO(this JobPutDTO dto, int JobId)
+        {
+            return new Job
+            {
+                Id = JobId,
+                Title = dto.Title,
+                Description = dto.Description,
+                Cost = dto.Cost,
+                Date = dto.Date,
+                DueDate = dto.DueDate,
+                Mileage = dto.Mileage,
+                DueMileage = dto.DueMileage
+            };
+        }
+    }
+}
---
+++ api/Helpers/Queries/JobQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Helpers.Queries
+{
+    public class JobQuery
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
