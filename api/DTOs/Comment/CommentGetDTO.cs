1eeeaf863099966cae53e315327e8fbc9ce16eda
Brambarov <ivan.brambarov@gmail.com>
2024-12-01 12:23:58 +0200

    Add Id property to GetDTOs

--- api/DTOs/Comment/CommentGetDTO.cs
+++ api/DTOs/Comment/CommentGetDTO.cs
@@ -2,6 +2,7 @@
 {
     public class CommentGetDTO
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; }
--- api/DTOs/User/UserGetDTO.cs
+++ api/DTOs/User/UserGetDTO.cs
@@ -4,6 +4,7 @@ namespace api.DTOs.User
 {
     public class UserGetDTO
     {
+        public string Id { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public List<MotorcycleGetDTO> Stable { get; set; } = [];
     }
