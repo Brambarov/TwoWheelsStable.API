a849aa558af15cc8d395703bba1fc1b228474594
Brambarov <ivan.brambarov@gmail.com>
2024-12-02 15:25:52 +0200

    Add Image model

---
+++ api/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public int? MotorcycleId { get; set; }
+        public Motorcycle? Motorcycle { get; set; }
+        public byte[]? Data { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string MimeType { get; set; } = string.Empty;
+    }
+}
