c6f389fa8944f6723510d26903b10cb43ca735e7
Brambarov <ivan.brambarov@gmail.com>
2024-12-02 23:29:01 +0200

    Create ImagesController. Implement BatchCreate endpoint

---
+++ api/Controllers/ImagesController.cs
@@ -0,0 +1,22 @@
+﻿using api.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController(IImagesService imagesService) : ControllerBase
+    {
+        private readonly IImagesService _imagesService = imagesService;
+
+        [HttpPost("{motorcycleId:guid}")]
+        public async Task<IActionResult> BatchCreate([FromRoute] Guid motorcycleId, [FromForm] List<IFormFile> files)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            await _imagesService.BatchCreateAsync(files, motorcycleId);
+
+            return Ok();
+        }
+    }
+}
