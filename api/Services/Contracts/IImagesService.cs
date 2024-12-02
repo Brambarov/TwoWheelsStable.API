f728f57581907742a04adb8e809ff6c296af12f4
Brambarov <ivan.brambarov@gmail.com>
2024-12-02 17:47:17 +0200

    Create ImagesService

---
+++ api/Services/Contracts/IImagesService.cs
@@ -0,0 +1,11 @@
+﻿using api.DTOs.Image;
+
+namespace api.Services.Contracts
+{
+    public interface IImagesService
+    {
+        Task<IEnumerable<ImageGetDTO>> GetByMotorcycleIdAsync(int motorcycleId);
+        Task<int?> CreateAsync(ImagePostDTO dto, int motorcycleId);
+        Task DeleteAsync(ImageGetDTO dto);
+    }
+}
---
+++ api/Services/ImagesService.cs
@@ -0,0 +1,27 @@
+﻿using api.DTOs.Image;
+using api.Helpers.Mappers;
+using api.Repositories.Contracts;
+using api.Services.Contracts;
+
+namespace api.Services
+{
+    public class ImagesService(IImagesRepository imagesRepository) : IImagesService
+    {
+        private readonly IImagesRepository _imagesRepository = imagesRepository;
+
+        public async Task<IEnumerable<ImageGetDTO>> GetByMotorcycleIdAsync(int motorcycleId)
+        {
+            return (await _imagesRepository.GetByMotorcycleIdAsync(motorcycleId)).Select(i => i.ToGetDTO());
+        }
+
+        public async Task<int?> CreateAsync(ImagePostDTO dto, int motorcycleId)
+        {
+            return await _imagesRepository.CreateAsync(dto.FromPostDTO(motorcycleId));
+        }
+
+        public Task DeleteAsync(ImageGetDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
