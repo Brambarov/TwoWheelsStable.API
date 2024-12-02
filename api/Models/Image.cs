27adc9d43cace75aec6d085577612b4e6685a930
Brambarov <ivan.brambarov@gmail.com>
2024-12-02 16:07:18 +0200

    Add IsDeleted to Image model

--- api/Models/Image.cs
+++ api/Models/Image.cs
@@ -8,5 +8,6 @@
         public byte[]? Data { get; set; }
         public string FileName { get; set; } = string.Empty;
         public string MimeType { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; }
     }
 }
