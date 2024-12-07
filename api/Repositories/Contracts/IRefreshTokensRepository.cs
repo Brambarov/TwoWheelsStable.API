79220f97b4b4c4e0d7109a2bc98e3d774941285b
Brambarov <ivan.brambarov@gmail.com>
2024-12-07 16:41:09 +0200

    Create RefreshTokensRepository with basic CRUD operations. Refactor UsersRepository

---
+++ api/Repositories/Contracts/IRefreshTokensRepository.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+
+namespace api.Repositories.Contracts
+{
+    public interface IRefreshTokensRepository
+    {
+        Task<IEnumerable<RefreshToken>> GetByUserIdAsync(string userId);
+        Task<RefreshToken> GetByTokenAsync(string token);
+        Task CreateAsync(RefreshToken refreshToken);
+        Task UpdateAsync(RefreshToken refreshToken);
+        Task DeleteAsync(RefreshToken refreshToken);
+    }
+}
