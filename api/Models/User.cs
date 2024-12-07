a7ac6f36d822476c61cc3796d3886babaa298d59
Brambarov <ivan.brambarov@gmail.com>
2024-12-07 16:40:14 +0200

    Create RefreshToken model. Add RefreshTokens collection property to User

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -4,6 +4,7 @@ namespace api.Models
 {
     public class User : IdentityUser
     {
+        public List<RefreshToken> RefreshTokens { get; set; } = [];
         public List<Motorcycle> Motorcycles { get; set; } = [];
         public bool IsDeleted { get; set; }
     }
