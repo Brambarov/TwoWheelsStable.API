279aed9bced6549f7d02880146adcd0d67e03126
Ivan Brambarov <ivan.brambarov@gmail.com>
2025-01-20 23:24:51 +0200

    Fix ci.yml (#5)
    * Change FakeItEasy with Moq nuget package for unit testing. Add MotorcyclesControllers tests
    
    * Update ci.yml to run on all branches. Add major & minor version incrementation
    
    * Fix ci.yml typo error
    
    * Update ci.yml
    
    * Fix bug in ci.yml
    
    * Fix bug in ci.yml
    
    * Fix bug in ci.yml

--- TwoWheelsStable.Test/Controllers/MotorcyclesControllerTests.cs
+++ TwoWheelsStable.Test/Controllers/MotorcyclesControllerTests.cs
@@ -1,6 +1,6 @@
-﻿using FakeItEasy;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Moq;
 using TwoWheelsStable.API.Controllers;
 using TwoWheelsStable.API.DTOs.Motorcycle;
 using TwoWheelsStable.API.Helpers.Queries;
@@ -10,64 +10,127 @@ namespace TwoWheelsStable.Test.Controllers
 {
     public class MotorcyclesControllerTests
     {
+        private readonly Mock<IMotorcyclesService> _mockMotorcyclesService;
+        private readonly Mock<ICommentsService> _mockCommentsService;
+        private readonly Mock<IJobsService> _mockJobsService;
+        private readonly Mock<IUrlHelperFactory> _mockUrlHelperFactory;
+        private readonly MotorcyclesController _motorcyclesController;
+
         private readonly Random random = new();
 
-        [Fact]
-        public async Task GetAll_Returns_OkResult_With_AllMotorcycles()
+        public MotorcyclesControllerTests()
         {
-            // Arrange
-            var expectedCount = random.Next(1, 100);
-            var motorcycleDTOs = A.CollectionOfDummy<MotorcycleGetDTO>(expectedCount).AsEnumerable();
-
-            var urlHelperFactory = A.Fake<IUrlHelperFactory>();
-            var motorcyclesService = A.Fake<IMotorcyclesService>();
-            var commentsService = A.Fake<ICommentsService>();
-            var jobsService = A.Fake<IJobsService>();
-
-            A.CallTo(() => motorcyclesService.GetAllAsync(A<MotorcycleQuery>._, A<IUrlHelper>._)).Returns(motorcycleDTOs);
-
-            var motorcyclesController = new MotorcyclesController(motorcyclesService,
-                                                                  commentsService,
-                                                                  jobsService,
-                                                                  urlHelperFactory);
-
-            // Act
-            var actionResult = await motorcyclesController.GetAll(new MotorcycleQuery());
-
-            // Assert
-            var result = Assert.IsType<OkObjectResult>(actionResult);
-            var motorcycles = Assert.IsType<List<MotorcycleGetDTO>>(result.Value);
-
-            Assert.NotNull(motorcycles);
-            Assert.Equal(expectedCount, motorcycles.Count);
+            _mockMotorcyclesService = new Mock<IMotorcyclesService>();
+            _mockCommentsService = new Mock<ICommentsService>();
+            _mockJobsService = new Mock<IJobsService>();
+            _mockUrlHelperFactory = new Mock<IUrlHelperFactory>();
+            _motorcyclesController = new MotorcyclesController(_mockMotorcyclesService.Object,
+                                                               _mockCommentsService.Object,
+                                                               _mockJobsService.Object,
+                                                               _mockUrlHelperFactory.Object);
         }
 
         [Fact]
-        public async Task GetById_Returns_OkResult_With_Motorcycle()
+        public async Task GetAll_Returns_200_With_ExpectedMotorcycleDTOs()
         {
             // Arrange
-            var motorcycleDTO = A.Fake<MotorcycleGetDTO>();
+            var expectedMotorcycleGetDTOs = GenerateMotorcycleGetDTOs(2, 20);
 
-            var urlHelperFactory = A.Fake<IUrlHelperFactory>();
-            var motorcyclesService = A.Fake<IMotorcyclesService>();
-            var commentsService = A.Fake<ICommentsService>();
-            var jobsService = A.Fake<IJobsService>();
-
-            A.CallTo(() => motorcyclesService.GetByIdAsync(A<Guid>._, A<IUrlHelper>._)).Returns(motorcycleDTO);
-
-            var motorcyclesController = new MotorcyclesController(motorcyclesService,
-                                                                  commentsService,
-                                                                  jobsService,
-                                                                  urlHelperFactory);
+            _mockMotorcyclesService.Setup(s => s.GetAllAsync(It.IsAny<MotorcycleQuery>(), It.IsAny<IUrlHelper>())).ReturnsAsync(expectedMotorcycleGetDTOs);
 
             // Act
-            var actionResult = await motorcyclesController.GetById(Guid.NewGuid());
+            var result = await _motorcyclesController.GetAll(It.IsAny<MotorcycleQuery>()) as OkObjectResult;
 
             // Assert
-            var result = Assert.IsType<OkObjectResult>(actionResult);
-            var motorcycle = Assert.IsType<MotorcycleGetDTO>(result.Value, false);
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(expectedMotorcycleGetDTOs, result.Value);
+        }
 
-            Assert.NotNull(motorcycle);
+        [Fact]
+        public async Task GetById_Returns_200_With_ExpectedMotorcycleDTO()
+        {
+            // Arrange
+            var motorcycleId = Guid.NewGuid();
+
+            var expectedMotorcycleGetDTO = GenerateMotorcycleGetDTO(motorcycleId);
+
+            _mockMotorcyclesService.Setup(s => s.GetByIdAsync(motorcycleId, It.IsAny<IUrlHelper>())).ReturnsAsync(expectedMotorcycleGetDTO);
+
+            // Act
+            var result = await _motorcyclesController.GetById(motorcycleId) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(expectedMotorcycleGetDTO, result.Value);
+        }
+
+        [Fact]
+        public async Task Create_Returns_200_With_ExpectedMotorcycleDTO()
+        {
+            // Arrange
+            var motorcyclePostDTO = new MotorcyclePostDTO
+            {
+                Name = "Motorcycle",
+                Make = "Make",
+                Model = "Model",
+                Year = 2000,
+                Mileage = 1000
+            };
+
+            var expectedMotorcycleGetDTO = new MotorcycleGetDTO
+            {
+                Name = motorcyclePostDTO.Name,
+                Make = motorcyclePostDTO.Make,
+                Model = motorcyclePostDTO.Model,
+                Year = motorcyclePostDTO.Year,
+                Mileage = motorcyclePostDTO.Mileage
+            };
+
+            _mockMotorcyclesService.Setup(s => s.CreateAsync(motorcyclePostDTO, It.IsAny<IUrlHelper>())).ReturnsAsync(expectedMotorcycleGetDTO);
+
+            // Act
+            var result = await _motorcyclesController.Create(motorcyclePostDTO) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(expectedMotorcycleGetDTO, result.Value);
+        }
+
+        private List<MotorcycleGetDTO> GenerateMotorcycleGetDTOs(int minValue, int maxValue)
+        {
+            int count = random.Next(minValue, maxValue);
+
+            var expectedMotorcycleDTOs = new List<MotorcycleGetDTO>();
+            for (int i = 0; i < count; i++)
+            {
+                expectedMotorcycleDTOs.Add(new MotorcycleGetDTO
+                {
+                    Href = $"mock/motorcycles/{i}",
+                    Name = $"Motorcycle{i}",
+                    Make = $"Make{i}",
+                    Model = $"Model{i}",
+                    Year = 2000 + i,
+                    Mileage = 1000 * i
+                });
+            };
+
+            return expectedMotorcycleDTOs;
+        }
+
+        private MotorcycleGetDTO GenerateMotorcycleGetDTO(Guid id)
+        {
+            return new MotorcycleGetDTO
+            {
+                Href = $"mock/motorcycles/{id}",
+                Name = $"Motorcycle{id}",
+                Make = $"Make{id}",
+                Model = $"Model{id}",
+                Year = 2000,
+                Mileage = 1000
+            };
         }
     }
 }
