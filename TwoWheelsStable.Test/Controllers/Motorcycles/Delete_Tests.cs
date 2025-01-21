a01595604d03fa03efae1fe907837557d4f5e6fc
Brambarov <ivan.brambarov@gmail.com>
2025-01-21 17:28:16 +0200

    Separate MotorcyclesController unit tests into different files. Add unit tests

---
+++ TwoWheelsStable.Test/Controllers/Motorcycles/Delete_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoWheelsStable.Test.Controllers.Motorcycles
+{
+    public class Delete_Tests : MotorcyclesController_Tests_Base
+    {
+    }
+}
